236198358aca0d3ec7698997c6f14c9d54cffc37
Rachael <rdougherty031@gmail.com>
2019-05-06 23:16:33 -0400

    testing2

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -19,7 +19,7 @@ var friendsArr = [{
 },
 {
     "name": "John",
-    "photo": "https://www.pexels.com/photo/adult-beard-boy-casual-220453/",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
         1,
         1,
